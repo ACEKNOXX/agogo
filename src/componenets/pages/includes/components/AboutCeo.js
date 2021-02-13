ace85887e77f9488024897e1a296dc49e21e0c93
Aceknoxx <mustapha.ddare@gmail.com>
2021-02-13 17:56:38 +0000

    changes

--- src/componenets/pages/includes/components/AboutCeo.js
+++ src/componenets/pages/includes/components/AboutCeo.js
@@ -11,23 +11,23 @@ export default function AboutCeo() {
                     </div>
                     <div className="col s12 m6 mt-25 ">
                         <h3 className="white-text">Meet the CEO</h3>
-                        <p className="grey-text mt-50" >
+                        <p className="grey-text text-lighten-4 mt-50" >
                                 Kehinde is a seasoned Finance professional and a strong player in the Treasury 
                                 Management space with over 21 years banking experience spanning Treasury , 
                                 Currency trading, Corporate Sales, Liquidity Management, Derivatives Trading, Financial Modeling,
                                 Fixed Income Trading and  Balance Sheet Management amongst other skills.
                         </p>
-                        <p className="grey-text " >
+                        <p className="grey-text text-lighten-4 " >
                                 She is an alumnus of the prestigious Goldman Sachs Training Institute, 
                                 Wharton Business School as well as the Lagos Business School (LBS).
                         </p>
-                        <p className="grey-text " >
+                        <p className="grey-text text-lighten-4 " >
                                 She is a member of the Lagos Business School Alumni Governing Council 
                                 and a Fellow of the Financial Markets Dealers Association. She is an Associate 
                                 of the Chartered Institute of Bankers Nigeria (CIBN) 
                                 as well as the Chartered Institute of Bankers Scotland and Wales.
                         </p>
-                        <p className="grey-text " >
+                        <p className="grey-text text-lighten-4 " >
                                 Her passion about seeing men and women take control of their 
                                 finances and live fulfilled lives led to her birthing AurorawithKWO 
                                 and eventually Aurora Financial Literacy Hub. This same passion led her in 
@@ -35,25 +35,25 @@ export default function AboutCeo() {
                                 developed to impart children with sound money and 
                                 finance skills needed for the future – “catching them young.
                         </p>
-                        <p className="grey-text">
+                        <p className="grey-text text-lighten-4">
                                 Aurora which means “new dawn” is a financial literacy platform designed to provide finance and career related guidance as well as  mentoring for individuals from the organized financial private sector, 
                                 business owners, budding and avid investors as well as all other works of life.    
                         </p>
-                        <p className="grey-text">
+                        <p className="grey-text text-lighten-4">
                                 This platform has hosted notable Business, Career and Thought 
                                 Leaders who have shared from their wealth of experience with 
                                 the members of the hub (fondly called “Tribers”) and has recorded 
                                 tremendous testimonials from members on how their finances, career and life have been transformed.
                         </p>
-                        <p className="grey-text">
+                        <p className="grey-text text-lighten-4">
                                 K.W.O as she is fondly called is a graduate of Economics and holds an MSC in Finance from the University of Leicester and MBA from the 
                                 University of Bangor both in the United Kingdom. She is also a member of the CFA Institute.
                         </p>
-                        <p className="grey-text">
+                        <p className="grey-text text-lighten-4">
                                 Kehinde is an Ordained Minister, a Wealth Coach, Teacher, Treasury and Investment Expert, 
                                 Financial Literacy Enthusiast and Advocate, Serial Entrepreneur and Business Owner
                         </p>
-                        <p className="grey-text">
+                        <p className="grey-text text-lighten-4">
                                 She is happily married to Wole Olomojobi. They are blessed with beautiful 3 children.
                         </p>
                     </div>
@@ -66,23 +66,23 @@ export default function AboutCeo() {
                         <div className="col s12 m5 mt-25 center " style={{ margin:" 0px auto !important" }}>
                         <h3 className="white-text ">Meet the CEO</h3>
                         
-                        <p className="grey-text mt-50" >
+                        <p className="grey-text text-lighten-4 mt-50" >
                                 Kehinde is a seasoned Finance professional and a strong player in the Treasury 
                                 Management space with over 21 years banking experience spanning Treasury , 
                                 Currency trading, Corporate Sales, Liquidity Management, Derivatives Trading, Financial Modeling,
                                 Fixed Income Trading and  Balance Sheet Management amongst other skills.
                         </p>
-                        <p className="grey-text " >
+                        <p className="grey-text text-lighten-4 " >
                                 She is an alumnus of the prestigious Goldman Sachs Training Institute, 
                                 Wharton Business School as well as the Lagos Business School (LBS).
                         </p>
-                        <p className="grey-text " >
+                        <p className="grey-text text-lighten-4 " >
                                 She is a member of the Lagos Business School Alumni Governing Council 
                                 and a Fellow of the Financial Markets Dealers Association. She is an Associate 
                                 of the Chartered Institute of Bankers Nigeria (CIBN) 
                                 as well as the Chartered Institute of Bankers Scotland and Wales.
                         </p>
-                        <p className="grey-text " >
+                        <p className="grey-text text-lighten-4 " >
                                 Her passion about seeing men and women take control of their 
                                 finances and live fulfilled lives led to her birthing AurorawithKWO 
                                 and eventually Aurora Financial Literacy Hub. This same passion led her in 
@@ -90,25 +90,25 @@ export default function AboutCeo() {
                                 developed to impart children with sound money and 
                                 finance skills needed for the future – “catching them young.
                         </p>
-                        <p className="grey-text">
+                        <p className="grey-text text-lighten-4">
                                 Aurora which means “new dawn” is a financial literacy platform designed to provide finance and career related guidance as well as  mentoring for individuals from the organized financial private sector, 
                                 business owners, budding and avid investors as well as all other works of life.    
                         </p>
-                        <p className="grey-text">
+                        <p className="grey-text text-lighten-4">
                                 This platform has hosted notable Business, Career and Thought 
                                 Leaders who have shared from their wealth of experience with 
                                 the members of the hub (fondly called “Tribers”) and has recorded 
                                 tremendous testimonials from members on how their finances, career and life have been transformed.
                         </p>
-                        <p className="grey-text">
+                        <p className="grey-text text-lighten-4">
                                 K.W.O as she is fondly called is a graduate of Economics and holds an MSC in Finance from the University of Leicester and MBA from the 
                                 University of Bangor both in the United Kingdom. She is also a member of the CFA Institute.
                         </p>
-                        <p className="grey-text">
+                        <p className="grey-text text-lighten-4">
                                 Kehinde is an Ordained Minister, a Wealth Coach, Teacher, Treasury and Investment Expert, 
                                 Financial Literacy Enthusiast and Advocate, Serial Entrepreneur and Business Owner
                         </p>
-                        <p className="grey-text">
+                        <p className="grey-text text-lighten-4">
                                 She is happily married to Wole Olomojobi. They are blessed with beautiful 3 children.
                         </p>                        
                     </div>
