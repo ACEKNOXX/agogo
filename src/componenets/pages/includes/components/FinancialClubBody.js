952029558ac7d0b43494e0c76062c21ec6b848a0
Aceknoxx <mustapha.ddare@gmail.com>
2021-02-13 17:42:45 +0000

    changes to form

--- src/componenets/pages/includes/components/FinancialClubBody.js
+++ src/componenets/pages/includes/components/FinancialClubBody.js
@@ -19,16 +19,20 @@ export default function FinancialClubBody(props) {
     const emailRef = useRef(null)
     const nameRef = useRef(null)
     const phoneNumberRef = useRef(null)
+    const investmentplanRef = useRef(null)
     const [ok, setOk] = useState(false)
+
     var dataToPush 
     const handleSubmit = (e) => {
         e.preventDefault();
     }
     const handleChange = (e) => {
-        var a, b, c;
+        var a, b, c, d;
         a = emailRef.current.value
         b = nameRef.current.value
         c = phoneNumberRef.current.value
+        d = investmentplanRef.current.value
+        amount = d
         if ( a.length !==0 && b.lenght !==0 && c.length !==0 ){
             setOk(true)
             dataToPush = {
@@ -38,6 +42,8 @@ export default function FinancialClubBody(props) {
                 amount:amount
             }
         } else {
+            console.log(d)
+            
             setOk(false)
         }
     }
@@ -56,7 +62,7 @@ export default function FinancialClubBody(props) {
                             <article className="grey-text">
                                 Seamlessly maximize user friendly communities whereas customer directed results.
                             </article><br />
-                            <form onSubmit={handleSubmit}>
+                            {/* <form onSubmit={handleSubmit}>
                                 <div className="form-section-1 mt-25 row">
                                     <div className=" col s12" >
                                         <label htmlFor="username" className="col s12 grey-text">Username</label>
@@ -90,7 +96,7 @@ export default function FinancialClubBody(props) {
                                     </div>
                                 </div>
 
-                            </form>
+                            </form> */}
                         </div>
                         <img src="assets/Aurora_images/Courses dashboard Aurora 1-min.png" 
                         className=" hide-on-med-and-down container z-depth-2 responsive-img full-w materialboxed show-on-med-and-down hide-on-med-and-up" width="100%"   alt="" />
@@ -110,8 +116,60 @@ export default function FinancialClubBody(props) {
                                 <div className="" style={{width:"400px"}}>
                                     <p className="grey-text">
                                         Seamlessly maximize user friendly communities whereas customer directed results.
-                                    </p><br/>
-                                    <PayButton amount={dataToPush} />
+                                    </p><br/> 
+                                    {/* <PayButton amount={dataToPush} /> */}
+                                     <form onSubmit={handleSubmit}>
+                                        <div className="form-section-1 mt-25 row">
+                                            <div className=" col s12" >
+                                                <label htmlFor="username" className="col s12 grey-text">Username</label>
+                                                <input onChange={handleChange} ref={nameRef} id="username" className="col s12 mt-50 ful l-w" placeholder="email address"  type="text" required/>
+                                            </div>
+                                        </div>
+                                        <div className="form-section-1 mt-25 row">
+                                            <div className=" col s12" >
+                                                <label htmlFor="email" className="col s12 grey-text">Email Address</label>
+                                                <input onChange={handleChange} id="email" className="col s12 mt-50 ful l-w" placeholder="email address"
+                                                    ref={emailRef} type="email" required/>
+                                            </div>
+                                        </div>
+                                        <div className="form-section-1 mt-25 row">
+                                            <div className=" col s12" >
+                                                <label htmlFor="phone" className="col s12 grey-text">Phone Number</label>
+                                                <input onChange={handleChange} id="phone" className="col s12 mt-50 ful l-w" placeholder="Phone number"
+                                                    ref={phoneNumberRef} type="tel" required/>
+                                            </div>
+                                        </div>
+                                        <div className="form-section-1 mt-25 row">
+                                            <div className="col s12">
+                                                <label>Investment club plan</label>
+                                                <select ref={investmentplanRef}  onChange={handleChange} className="browser-default">
+                                                    {financialPlan === "Investment Club" && 
+                                                        <option value="10000">₦10000/12months</option>
+                                                    }
+                                                    {financialPlan === "Investment Club" && 
+                                                        <option value="6000">₦6000/6months</option>
+                                                    }
+                                                    {financialPlan  === "Savings club" &&
+                                                        <option value="5000">₦5000/One-off Fee</option>
+                                                    }
+                                                   
+                                                </select>
+                                            </div>
+                                        </div>
+                                        <div className="form-section-1 mt-25 row">
+                                            <div className=" col s12" >
+                                                {!ok &&
+                                                    <button type="submit" className="btn-flat btn-primary green darken-4 white-text"
+                                                    >Proceed to Pay({amount})</button>
+                                                }
+                                                
+                                                {ok &&
+                                                    <PayButton amount={dataToPush} />
+                                                }
+                                            </div>
+                                        </div>
+
+                                    </form>
                                 </div>
                             </div>
                             
