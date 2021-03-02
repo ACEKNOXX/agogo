5c5365ddea12a8621bc5a767c2e80020a7fdf0dc
Aceknoxx <mustapha.ddare@gmail.com>
2021-03-02 15:25:10 +0000

    hcanges

--- src/componenets/pages/includes/components/Feedback.js
+++ src/componenets/pages/includes/components/Feedback.js
@@ -1,6 +1,17 @@
 import React from 'react'
 
 export default function Feedback() {
+    var items = [
+        {
+            name: "Georgie O.",
+            description: "Thank you KWO for all your work with us ! 🙏🏾 Thank you all my fellow jade aurora tribers for making this work seamlessly 🙏🏾."
+        },
+        {
+            name: "Esther Kalu.",
+            description: 'Thank you so much mama,  this savings group really helped us this year.I am extremely grateful and we wish we do more next year.'
+        }
+    ]
+
     return (
         <div className=" pt-50 full-w" id="feedbacks">
             <div className="row hide-on-med-and-down">
@@ -14,38 +25,39 @@ export default function Feedback() {
                 </div>
             </div>
 
+            {
+                items.map( (item, i) => <Item key={i} item={item} /> )
+            }
+            
+
+        </div>
+    )
+}
+
+function Item(props)
+{
+    return (
+        <div className="center">
+            
+            {/* <img src={props.item.url} width="100%"  alt=""/>
+            <h4>{props.item.title}</h4> */}
+            {/* <p>{props.item.description}</p> */}
             <div className=" row mb-25  container" id="feedback-tab" >
                 <div className="col  s12 center m3">
                     <div className="img-avatar" >
                         <img className="" src="assets/Aurora_images/Ellipse 19-min.png"  width="100px" height="100px"  alt="" />
                     </div>
-                    <h6> <b> Godswill Wike</b> </h6>
+                    <h6> <b>{ props.item.name}</b> </h6>
                     <small>Student</small>
                 </div>
                 <div className="col  s12 m7">
                     <article className="pt-50">
-                        Seamlessly maximize user friendly communities whereas customer directed results. Professionally productize impactful portals through standardized ROI. Energistically initiate client-centric results with value-added vortals.
+                        {props.item.description}
                     </article>
                 </div>
                 <div className="col  s12 m2"></div>
             </div>
             <br/>
-            <div className="row   container mb-25"  id="feedback-tab">
-                <div className="col  s12 center m3">
-                    <div className="img-avatar" >
-                        <img  src="assets/Aurora_images/Ellipse 19-min.png"  width="100px" height="100px"  alt="" />
-                    </div>
-                    <h6> <b> Godswill Wike</b> </h6>
-                    <small>Student</small>
-                </div>
-                <div className="col  s12 m7">
-                    <article className="pt-50">
-                        Seamlessly maximize user friendly communities whereas customer directed results. Professionally productize impactful portals through standardized ROI. Energistically initiate client-centric results with value-added vortals.
-                    </article>
-                </div>
-                <div className="col  s12 m2"></div>
-            </div>
-
         </div>
     )
 }
--- src/componenets/pages/includes/components/HowItWorks.js
+++ src/componenets/pages/includes/components/HowItWorks.js
@@ -24,11 +24,11 @@ export default function HowItWorks() {
                                 <button className="btn-floating btn-large waves-effect waves-light" style={{ background:"#002044"}}>
                                     01
                                 </button>
-                                <h6><b> Sign Up</b></h6>
+                                <h6><b>PICK A PLAN</b></h6>
                             </div>
                             <div className="container" style={{width:"200px !important"}}>
                                 <span className="black-text " >
-                                    The first step is to sign up on our platform with your email.
+                                    Select a savings or investment plan that suits your goal.
                                 </span>
                             </div>
                             
@@ -42,10 +42,10 @@ export default function HowItWorks() {
                                 <button className="btn-floating btn-large waves-effect waves-light" style={{ background:"#002044"}}>
                                     02
                                 </button>
-                                <h6><b>Choose A Plan</b></h6>
+                                <h6><b>SIGN UP</b></h6>
                             </div>
                             <span className="black-text">
-                                Choose a plan that suits your goal from our 6 months and 12 months plan that provide you access to all our resources with  differing timeframes.
+                                Sign up on our platform with your details.
                             </span>
                         </div>
                     </div>
@@ -57,20 +57,21 @@ export default function HowItWorks() {
                                 <button className="btn-floating btn-large waves-effect waves-light" style={{ background:"#002044"}}>
                                     03
                                 </button>
-                                <h6><b> Get Certificate</b></h6>
+                                <h6><b> GET STARTED</b></h6>
                             </div>
                             
                             <div className="container" style={{ width: "200px !important "}}>
                                 <span className="black-text">
-                                    You’ll get certificates at the completion of each course.
+                                    Join a group based on your goals and start saving/investing.
                                 </span>
                             </div>
                         </div>
                     </div>
 
                 </div>
+         
                 <div className="container row">
-                    <div className="col s12 center">
+                    {/* <div className="col s12 center">
                         <p>
                             <b>
                                 Kickstart your journey to financial literacy by registering on Aurora.
@@ -81,11 +82,9 @@ export default function HowItWorks() {
                                 The turning point is one click away. Join now!
                             </b>
                         </p>
-                    </div>
+                    </div> */}
                     <div className="col s12 center ">
-                            {/* <button type="button"  data-target="modal-pricing-plan" className="btn-large btn-flat btn-large-primary white-text green modal-trigger">
-                        Join aurora 
-                            </button> */}
+                        <br/>
                         <PricePlan />
                     </div>
                 </div>
